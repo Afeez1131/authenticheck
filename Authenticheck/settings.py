ad9fed5290b65f7c3bab8eacb5f06ab5780f952e
afeez1131 <lawalafeez052@gmail.com>
2024-01-22 15:26:09 +0100

    started dashboard implementation

--- Authenticheck/settings.py
+++ Authenticheck/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'Authenticheck.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'Authenticheck/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,7 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
